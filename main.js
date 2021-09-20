c31303cde60283725113629be2aa3d4bb3dcdbc6
Dabsia <dabojohnson98@gmail.com>
2021-09-20 10:51:49 +0100

    Made some updates on the chat page

--- main.js
+++ main.js
@@ -13,10 +13,11 @@ function submitForm(e){
     if (chat_name.value.length > 0 && room_name.value.length > 0){
         localStorage.setItem('room_name', room_name.value)
         localStorage.setItem('chat_name', chat_name.value)
-        window.location.href = window.location.href.replace('index.html', 'chat.html');      
+        window.location.href = window.location.href.replace('chat.html');      
     }else{
         chat_input.style.border = '1px solid red' 
     }
+    
    
 }
 let main = document.querySelector('main')
